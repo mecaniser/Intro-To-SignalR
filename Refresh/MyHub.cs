487cb1ada300c2f50bad6db3bdc484098eceb911
Sergio <sergioburca@gmail.com>
2018-07-12 06:08:40 -0400

    initial commit of my first touch on SignalR!

---
+++ Refresh/MyHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Refresh
+{
+    public class MyHub : Hub
+    {
+        public void Hello() => Clients.All.MessageReciever(DateTime.UtcNow);
+
+        public void MessageAllTime() => Clients.All.MessageReciever(DateTime.Today);
+    }
+}
---
+++ Refresh/Scripts/signalR.js
@@ -0,0 +1,22 @@
+﻿
+
+//Immediate Invoked Function
+
+(function () {
+    $.connection.hub.start()
+        .done(function () {
+            console.log("We have connection, on console!");
+
+            // ReSharper disable once PossiblyUnassignedProperty
+            //$.connection.myHub.server.hello(); 
+            $.connection.myHub.server.messageAllTime();
+
+        })
+        .fail(function () { alert("There is an Error, inspect your code") });
+
+    $.connection.myHub.client.MessageReciever = function (message) {
+        $("#message").append(message);
+        $("#message2").append(message);
+        console.log(message);
+    }
+})()
